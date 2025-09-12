d248f1abe259fb9116ddc86f38e6a371a76df135
candradehuetio <carlosandresandradehuetio@gmail.com>
2025-09-12 16:16:27 +0200

    Update of the website completing static requirements

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,29 +1,7 @@
-// This is the initial call out of the variable
-const currentdate = document.querySelector("#currentyear");
 
-// Variable for the current date
-const today = new Date();
+// Set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-/* The green text is a string that is deployed in the element with currentyear ID
-by the "innerHTML" property. */
-currentdate.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(today)} </span>`;
+// Set last modified date
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
 
-
-// Last modify
-const pattern = /last_modif\s*=\s*([^;]*)/;
-
-const lastVisit = parseFloat(document.cookie.replace(pattern, "$1"));
-const lastModif = Date.parse(document.lastModified);
-
-if (Number.isNaN(lastVisit) || lastModif > lastVisit) {
-    document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname
-        }`;
-
-    if (isFinite(lastVisit)) {
-        
-        // Replaced the alert to the innerHTML property to insert a span with the date of change
-        const currentModify = document.querySelector("#lastModified");
-        const dateModify = new Date();
-        currentModify.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(dateModify)} </span>`;
-    }
-}
