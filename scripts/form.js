834be03a8dd50105056d97f630bdb07dab0b761a
candradehuetio <carlosandresandradehuetio@gmail.com>
2025-10-09 00:24:10 +0200

    audit tool

--- scripts/form.js
+++ scripts/form.js
@@ -21,3 +21,7 @@ products.forEach(product => {
     select.appendChild(option);
 });
 
+// Set the current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
